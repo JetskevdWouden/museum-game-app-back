0d15b1692749e26601180bba6dfb769ae76b0db8
JetskevdWouden <zegmaarjet@gmail.com>
2019-07-08 17:07:18 +0200

    sorry added straight to dev. router for new user

---
+++ user/router.js
@@ -0,0 +1,44 @@
+const {Router} = require('express');
+const bcrypt = require('bcrypt');
+const User = require('../user/model');
+const auth = require('../auth/middleware');
+
+const router = new Router();
+
+//NEW USER --> SIGN UP at "/users" --> can't at "/" because already has a POST route for login
+router.post('/users', auth, (req, res, next) => {
+    const user = {
+        username: req.body.username,
+        password: req.body.password,
+        password_confiramtion: req.body.password_confiramtion
+    }
+    if (user.username && user.password && user.password_confirmation) {
+        user.password = bcrypt.hashSync(req.body.password, 10)
+        if (bcrypt.compareSync(user.password_confirmation, user.password)) {
+            User
+                .create(user)
+                .then(user => {
+                    res
+                        .status(201)
+                        .send({
+                            message: "A NEW USER WAS CREATED",
+                            username: user.username,
+                            user_id: user.id
+                        })
+                })
+                .catch(error => next(error))
+        } else {
+            res
+                .status(422)
+                .send({
+                    message: "PLEASE MAKE SURE YOUR PASSWORDS MATCH"
+                })
+        }
+    } else {
+        res
+            .status(400)
+            .send({
+                message: "PLEASE FILL IN ALL REQUIRED FIELDS"
+            })
+    }
+})
