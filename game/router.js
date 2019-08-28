7248be90224af57c49e63b9b75bb74700d9ffaf6
JetskevdWouden <zegmaarjet@gmail.com>
2019-08-28 22:51:33 +0200

    game router cleaned up comments for consistency

--- game/router.js
+++ game/router.js
@@ -6,15 +6,14 @@ const Score = require('../score/model');
 
 const router = new Router();
 
-//user creates new game
-//update user.gameId to new gameId
-//create 1 score entity
+//GET - create new game
 router.post('/new-game', auth, (req, res, next) => {
     const userId = req.user.id
     const game = {
         open: true,
         active: true
     }
+
     Game
         .create(game)
         .then(game => {
@@ -27,20 +26,16 @@ router.post('/new-game', auth, (req, res, next) => {
                 })
                 .then(user => {
                     user
-                        .update({
-                            gameId: gameId
-                        })
+                        .update({ gameId })
                     Score
                         .create({
                             userId: userId,
                             gameId: user.gameId
                         })
                         .then(entity => {
-                            res
-                                .send({
-                                    message: "hello"
-                                })
+
                         })
+                        .catch(error => next(error))
                 })
                 .catch(error => next(error))
             res
@@ -53,7 +48,7 @@ router.post('/new-game', auth, (req, res, next) => {
         .catch(error => next(error))
 })
 
-//get all open games
+//GET - return all games where open === true
 router.get('/open-games', auth, (req, res, next) => {
     Game
         .findAll({
@@ -73,9 +68,10 @@ router.get('/open-games', auth, (req, res, next) => {
         .catch(error => next(error))
 })
 
-//get ---> game by ID
+//GET - game by id
 router.get('/game/:id', auth, (req, res, next) => {
     const gameId = req.params.id
+
     Game
         .findByPk(gameId)
         .then(game => {
@@ -103,9 +99,11 @@ router.get('/game/:id', auth, (req, res, next) => {
         .catch(error => next(error))
 })
 
-//put --> update open game to closed game
+//ENDPOINT NOT IN USE
+//PUT - update game open => false
 router.put('/closed-game', auth, (req, res, next) => {
-    const gameId = req.body.gameId      //send game id to close in body
+    const { gameId } = req.body
+
     Game
         .findByPk(gameId)
         .then(game => {
@@ -114,7 +112,7 @@ router.put('/closed-game', auth, (req, res, next) => {
                     open: false
                 })
             res
-                .status(200)            //corrent HTTP code?
+                .status(200)
                 .send({
                     message: `GAME WITH ID ${gameId} HAS BEEN CLOSED`,
                     game: game
@@ -123,9 +121,11 @@ router.put('/closed-game', auth, (req, res, next) => {
         .catch(error => next(error))
 })
 
-//put --> update active game to finished game
+//ENDPOINT NOT IN USE
+//PUT - update game active => false
 router.put('/finished-game', auth, (req, res, next) => {
-    const gameId = req.body.gameId      //send game id to deactivate in body
+    const { gameId } = req.body
+
     Game
         .findByPk(gameId)
         .then(game => {
@@ -134,7 +134,7 @@ router.put('/finished-game', auth, (req, res, next) => {
                     active: false
                 })
             res
-                .status(200)            //corrent HTTP code?
+                .status(200)
                 .send({
                     message: `GAME WITH ID ${gameId} HAS BEEN DEACTIVATED`,
                     game: game
